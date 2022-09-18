744217c5924d9542e9de854f60c469984d88839a
railsdev <railsdev@localhost.localdomain>
2022-09-18 13:24:38 +0900

    * ユーザ一覧画面のカードコンポーネント作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,17 @@
+import { Wrap, WrapItem } from '@chakra-ui/react'
 import { memo, FC } from 'react'
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: FC = memo(() => {
   return (
-    <div>UserManagementページです</div>
+    <Wrap p={{base: 4, md: 10}}>
+      <WrapItem>
+        <UserCard
+          imageUrl={'https://source.unsplash.com/random'}
+          userName={'Katz0726'}
+          userFullName={'Katz 0726'}
+        />
+      </WrapItem>
+    </Wrap>
   )
 });
