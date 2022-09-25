8b9a9c947efebf8ea3e0c6f0969993747d238543
railsdev <railsdev@localhost.localdomain>
2022-09-25 22:44:40 +0900

    ヘッダメニューの遷移できない不具合を修正

--- src/components/organisms/layouts/Header.tsx
+++ src/components/organisms/layouts/Header.tsx
@@ -1,4 +1,4 @@
-import { memo, FC } from 'react';
+import { memo, FC, useCallback } from 'react';
 import {
   Flex,
   Heading,
@@ -15,9 +15,9 @@ export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const navigate = useNavigate();
 
-  const onClickHome = navigate("/home");
-  const onClickUserManagement = navigate("/home/user_management");
-  const onClickSettings = navigate("/home/setting");
+  const onClickHome = useCallback(() => navigate("/home"), [navigate]);
+  const onClickUserManagement = useCallback(() => navigate("/home/user_management"), [navigate]);
+  const onClickSettings = useCallback(() => navigate("/home/setting"), [navigate]);
 
   return (
     <>
@@ -34,24 +34,24 @@ export const Header: FC = memo(() => {
           as="a"
           mr={8}
           _hover={{ cursor: "pointer" }}
-          onClick={ () => onClickHome }
+          onClick={ onClickHome }
         >
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザ管理アプリ</Heading>
         </Flex>
         <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
           <Box pr={4}>
-            <Link onClick={ () => onClickUserManagement }>ユーザ一覧</Link>
+            <Link onClick={ onClickUserManagement }>ユーザ一覧</Link>
           </Box>
-          <Link onClick={ () => onClickSettings }>設定</Link>
+          <Link onClick={ onClickSettings }>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
       <MenuDrawer
         onClose={onClose}
         isOpen={isOpen}
-        onClickHome={() => onClickHome}
-        onClickSetting={() => onClickSettings}
-        onClickUserManagement={() => onClickUserManagement}
+        onClickHome={onClickHome}
+        onClickSetting={onClickSettings}
+        onClickUserManagement={onClickUserManagement}
       />
     </>
   )
